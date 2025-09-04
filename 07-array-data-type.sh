63773a50257294659d511f914e23ca8708c7414c
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-04 21:29:36 +0530

    Array data type of movies

--- 07-array-data-type.sh
+++ 07-array-data-type.sh
@@ -1,6 +1,11 @@
 #!bin/bash
-Movies={"COURT" "HIT" "PUSHPA2" "THANDEL" "JANTA"}
+Movies=("COURT" "HIT" "PUSHPA2" "THANDEL" "JANTA")
 
 echo "First Movie: ${Movie[0]}"
+echo "First Movie: ${Movie[5]}"
+
+
+
+echo "All Movies: ${Movie[@]}"
 
 
