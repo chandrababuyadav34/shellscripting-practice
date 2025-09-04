c47a5df08a2c5373aecd839c95e71bbe40ef02e3
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-04 20:35:22 +0530

    variables1

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Modi
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am doing doinG Grea. Thanks for asking. BTW How are you doing"
+echo "$PERSON1:: I am doing Good too! Whats up"
+echo "$PERSON2:: All Good from my side!"
