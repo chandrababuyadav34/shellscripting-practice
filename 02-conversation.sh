1774ca6f338710a73d5e624f70765c92ef857f2d
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-04 20:29:42 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Mr.Trump, I am doing doinG Grea. Thanks for asking. BTW How are you doing"
+echo "Trump:: I am doing Good too! Whats up"
+echo "Musk:: All Good from my side!"
