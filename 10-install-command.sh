635cbc79828b9bfe84ca3f56962ee5911a5ab565
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-05 18:09:20 +0530

    ch

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -2,9 +2,18 @@
 
 USERID=$(id -u)
 
-if [ USERID -ne 0]
+if [ USERID -ne 0 ]
 then
     echo "ERROR :: Please run the script with root access"
 else
     echo "You are running with root access"
 fi
+
+dng install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installation of Mysql is... SUCCESS"
+else
+    echo "Installation of Mysql is... FAILURE"
+fi    
