c1d182558d2b5db2024db1c03885698aaa15edc6
chandrababuyadav34 <splendidchandu34@gmail.com>
2025-09-05 18:34:32 +0530

    mysql install command through conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+NUMBER=$1
+
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal
+# -ne --> not equal
+
 if [ $NUMBER -lt 10 ]
 then
-    echo "Given Number $NUMBER is less than 10"
+    echo "Given number $NUMBER is less than 10"
 else
-    echo "Given Number $NUMBER is not less than 10"
+    echo "Given number $NUMBER is not less than 10"
 fi
